6f3631a8bba8203146994d2f410c4e80be955462
Nizam <ramazanov570633@gmail.com>
2023-09-30 15:24:18 +0400

    Home end0

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="SplasScreen" component={SplasScreen} /> */}
+        <Stack.Screen name="SplasScreen" component={SplasScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
